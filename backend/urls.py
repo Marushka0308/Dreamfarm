e99879794bacae57239b5c101ad35b1ecaec762f
Marushka Fernandes <marusfern@gmail.com>
2024-03-02 04:01:09 +0530

    SDA

--- backend/urls.py
+++ backend/urls.py
@@ -4,12 +4,14 @@ from backend import signup_views
 from backend import login_views
 from backend import marketplace
 from backend import comments
+from backend import community
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", signup_views.sign_up),
     path("login/",login_views.login, name = 'login'),
-    path("marketplace/", marketplace.product),
+    path("marketplace/", marketplace.product),git
     path("comments/", comments.comment),
+    path("community/",community.post),
 
 ]
